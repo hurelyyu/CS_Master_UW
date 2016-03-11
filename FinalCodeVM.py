7afa7ef1d4649a3f901695f1442485da7b40b745
Hurely Yu <yaqunyu@uw.edu>
2016-03-11 15:25:29 -0800

    Update FinalCodeVM.py

--- FinalCodeVM.py
+++ FinalCodeVM.py
@@ -167,7 +167,7 @@ X_train, X_test, y_train, y_test = train_test_split(
      X, y, test_size=0, random_state=42)
 n_components = X_train.shape[0]
 t0 = time()
-neigh = KNeighborsClassifier(n_neighbors=10)
+neigh = KNeighborsClassifier(n_neighbors=90)
 clf = neigh.fit(X_train, y_train)
 # print("Best estimator found by grid search:") #Best estimator found by grid search:
 # print(clf.best_estimator_) 
